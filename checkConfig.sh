ea44453a7b5985134ff99483b2b9a949040cc4d1
Kristoffer Framstad <stoffer@linus.lan>
2018-12-09 21:58:45 +0000

    Add correct path to HA config

--- checkConfig.sh
+++ checkConfig.sh
@@ -1,2 +1,2 @@
-docker run -it --rm -v /Users/fichfram/docker-compose/Home-AssistantConfig:/config -e "TZ=Europe/Oslo" homeassistant/home-assistant python -m homeassistant --config /config --script check_config
+docker run -it --rm -v /home/stoffer/smartHome/docker-compose/Home-AssistantConfig:/config -e "TZ=Europe/Oslo" homeassistant/home-assistant python -m homeassistant --config /config --script check_config
 
